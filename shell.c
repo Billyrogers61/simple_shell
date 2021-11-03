cb949a0aabdd410b2d64a2f92164860c51a309a1
Billyrogers61 <rogerskim61@gmail.com>
2021-11-03 05:28:38 -0700

    Simple shell program

--- shell.c
+++ shell.c
@@ -1,15 +1,6 @@
 #include <stdio.h>
 #include "main.h"
 
-#define MAX_SIZE_CMD 256
-#define MAX_SIZE_ARG 16
-
-/* declaration of variables */
-char cmd[MAX_SIZE_CMD];
-char *argv[MAX_SIZE_ARG];
-pid_t pid;
-char i;
-
 /**
  * main - Entry point to the main function.
  *
@@ -17,11 +8,11 @@ char i;
  * Return: 0 if scuccessful.
  */
 
-int main()
+int main(void)
 {
 	/* tie the handler to the SGNCHILD signal */
 	signal(SIGCHILD, log_handle);
 	/* starts the shell */
 	c_shell();
-	return 0;
+	return (0);
 }
